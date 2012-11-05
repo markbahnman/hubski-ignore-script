f2b182c5152853e2e19a17464baacccf0fc0d2aa
Mark Bahnman <mark.bahnman@gmail.com>
2012-11-04 22:22:44 -0800

    Formatting updated, changed metadata to have include instead of match as it was not being recognized as a greasemonkey script and changing it seemed to fix it

--- Hubski_Ignore_Modifying_Script.user.js
+++ Hubski_Ignore_Modifying_Script.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
-// @name Hubski Ignore Modifying Script
-// @namespace hubski.com/feed?id=forwardslash
-// @version 1.0
-// @description This is a script to remove any posts from ignored users or tags from your hubski feed
-// @run-at document-end
-// @grant none
-// @match http*://hubski.com/*
+// @name	Hubski Ignore Modifying Script
+// @description	This is a script to remove any posts from ignored users or tags from your hubski feed
+// @version	1.0
+// @namespace	hubski.com/feed?id=forwardslash
+// @run-at	document-end
+// @grant	none
+// @match	http*://hubski.com/*
 // ==/UserScript==
 
 
 var elem = document.getElementsByClassName('box');
+
 for(var i=0;i<elem.length;i++)
 {
 
