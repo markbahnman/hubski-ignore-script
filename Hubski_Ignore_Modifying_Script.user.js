49f3b439a9baf6f57a0e8ff129410bf1779235bf
Mark Bahnman <mark.bahnman@gmail.com>
2012-11-05 09:56:18 -0800

    Added comment removal from ignored users.

--- Hubski_Ignore_Modifying_Script.user.js
+++ Hubski_Ignore_Modifying_Script.user.js
@@ -1,23 +1,34 @@
 // ==UserScript==
 // @name	Hubski Ignore Modifying Script
 // @description	This is a script to remove any posts from ignored users or tags from your hubski feed
-// @version	1.0
+// @version	1.1
 // @namespace	hubski.com/feed?id=forwardslash
 // @run-at	document-end
 // @grant	none
 // @include	http*://hubski.com/*
 // ==/UserScript==
 
+//Select all posts and comments by class name
+var posts = document.getElementsByClassName('box');
+var comments = document.getElementsByClassName('outercomm');
 
-var elem = document.getElementsByClassName('box');
-
-for(var i=0;i<elem.length;i++)
-{
-
-var ignored = elem[i].innerHTML.search('a90101\">');
-	if (ignored >= 0) {
-		elem[i].style.display='none';
+//Filter comments and posts by seeing if a90101"> occurs within the div tags 
+if (comments.length > 0) {
+	for(var i = 0;i<comments.length;i++) {
+		var ignoredComments = comments.innerHTML.search('a90101\">');
+		if (ignoredComments >= 0) {
+			comments[i].style.display='none';
+		}
 	}
 }
-    
+
+if (posts.length > 0) {
+	for(var i=0;i<posts.length;i++)
+	{
+		var ignored = posts[i].innerHTML.search('a90101\">');
+		if (ignored >= 0) {
+			posts[i].style.display='none';
+		}
+	}
+}
 
