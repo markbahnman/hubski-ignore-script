844587de42335ae7e38b53f8840c71051107113a
Mark Bahnman <mark.bahnman@gmail.com>
2012-11-07 20:54:29 -0800

    Removing post code as it is being implemented by hubski

--- Hubski_Ignore_Modifying_Script.user.js
+++ Hubski_Ignore_Modifying_Script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name	Hubski Ignore Modifying Script
 // @description	This is a script to remove any posts from ignored users or tags from your hubski feed
-// @version	1.1
+// @version	1.2
 // @namespace	hubski.com/feed?id=forwardslash
 // @run-at	document-end
 // @grant	none
@@ -9,7 +9,6 @@
 // ==/UserScript==
 
 //Select all posts and comments by class name
-var posts = document.getElementsByClassName('box');
 var comments = document.getElementsByClassName('outercomm');
 
 //Filter comments and posts by seeing if a90101"> occurs within the div tags 
@@ -18,16 +17,6 @@ if (comments.length > 0) {
 		var ignoredComments = comments.innerHTML.search('a90101\">');
 		if (ignoredComments >= 0) {
 			comments[i].style.display='none';
-		}
-	}
-}
-
-if (posts.length > 0) {
-	for(var i=0;i<posts.length;i++)
-	{
-		var ignored = posts[i].innerHTML.search('a90101\">');
-		if (ignored >= 0) {
-			posts[i].style.display='none';
 		}
 	}
 }
